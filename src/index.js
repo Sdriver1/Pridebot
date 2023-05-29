5bef5af91f323c3f16493bf6485d0d6fe07ce6b0
Sdriver1 <sjdriver08@gmail.com>
2023-05-29 01:45:37 -0400

    Add bot join/leave logging

--- src/index.js
+++ src/index.js
@@ -1,7 +1,13 @@
 require("dotenv").config();
 const { token, databaseToken } = process.env;
 const { connect } = require("mongoose");
-const { Client, Collection, GatewayIntentBits } = require("discord.js");
+const {
+  Client,
+  Collection,
+  GatewayIntentBits,
+  Events,
+  EmbedBuilder,
+} = require("discord.js");
 const fs = require("fs");
 
 const client = new Client({
@@ -43,3 +49,63 @@ client.login(token);
 (async () => {
   await connect(databaseToken).catch(console.error);
 })();
+
+client.on(Events.GuildCreate, async (guild) => {
+  const channel = await client.channels.cache.get("1112590962867310602");
+  const name = guild.name;
+  const serverID = guild.id;
+  const memberCount = guild.memberCount;
+  const ownerID = guild.ownerId;
+  const owner = await client.users.cache.get(ownerID);
+  const ownerName = owner.username;
+  const serverBoost = guild.premiumSubscriptionCount;
+
+  const currentGuildCount = client.guilds.cache.size;
+
+  const embed = new EmbedBuilder()
+    .setColor(0xff00ae)
+    .setTitle(`👋 New Server Joined`)
+    .setFields({
+      name: "<:_:1112602480128299079> Server Info",
+      value: `**Server Name:** ${name} (${serverID})\n**Server Owner:** <@${ownerID}> (${ownerName} / ${ownerID}) \n**Member Count:** ${memberCount} \n**Boosts:** ${serverBoost}/14 \n**Server Creation:** <t:${parseInt(guild.createdTimestamp / 1000)}:F> (<t:${parseInt(guild.createdTimestamp / 1000)}:R>)`,
+    },
+    {
+      name: "<:_:1112602480128299079> Bot Info",
+      value: `**Total # of guild:** ${currentGuildCount}`,
+    })
+    .setTimestamp()
+    .setFooter({ text: `${serverID}` });
+
+  await channel.send({ embeds: [embed] });
+});
+
+client.on(Events.GuildDelete, async (guild) => {
+  const channel = await client.channels.cache.get("1112590962867310602");
+  const name = guild.name;
+  const serverID = guild.id;
+  const memberCount = guild.memberCount;
+  const ownerID = guild.ownerId;
+  const owner = await client.users.cache.get(ownerID);
+  const ownerName = owner.username;
+  const serverBoost = guild.premiumSubscriptionCount;
+
+  const currentGuildCount = client.guilds.cache.size;
+
+  const embed = new EmbedBuilder()
+    .setColor(0xff00ae)
+    .setTitle(`❌ Left Server`)
+    .setFields({
+      name: "<:_:1112602480128299079> Server Info",
+      value: `**Server Name:** ${name} (${serverID})\n**Server Owner:** <@${ownerID}> (${ownerName} / ${ownerID}) \n**Member Count:** ${memberCount} \n**Boosts:** ${serverBoost}/14 \n**Server Creation:** <t:${parseInt(
+        guild.createdTimestamp / 1000
+      )}:R> \n**Joined:** <t:${parseInt(guild.joinedTimestamp / 1000)}:F> (<t:${parseInt(guild.joinedTimestamp / 1000)}:R>)`, 
+    },
+    {
+      name: "<:_:1112602480128299079> Bot Info",
+      value: `**Total # of guild:** ${currentGuildCount}`,
+    })
+    .setTimestamp()
+    .setFooter({ text: `${serverID}` });
+
+  await channel.send({ embeds: [embed] });
+});
