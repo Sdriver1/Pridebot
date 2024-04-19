54c3631c6913a440f0cef68a681fd44ce20a4eb3
Sdriver1 <sjdriver08@gmail.com>
2024-04-19 15:31:23 -0400

    Updated Stats.js

--- src/commands/tools/stats.js
+++ src/commands/tools/stats.js
@@ -1,11 +1,11 @@
 require("dotenv").config();
-const { githubToken, apikey } = process.env;
+const axios = require("axios");
+const chalk = require("chalk");
+const os = require("os");
 const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 const { calculatePing } = require("../../events/client/ping");
-const axios = require("axios");
-const Nodeactyl = require("nodeactyl");
-const chalk = require("chalk");
 const CommandUsage = require("../../../mongo/models/usageSchema");
+const Profile = require("../../../mongo/models/profileSchema");
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -24,254 +24,159 @@ module.exports = {
         `-------------------------- \n/stats \nServer: ${interaction.guild.name} (${interaction.guild.id}) \nUser: ${interaction.user.tag} (${interaction.user.id}) \nTime: ${estDate} (EST) \n--------------------------`
       )
     );
+
+    function getCpuUsage() {
+      return new Promise((resolve) => {
+        const start = process.cpuUsage();
+        const startTime = Date.now();
+
+        setTimeout(() => {
+          const end = process.cpuUsage(start);
+          const endTime = Date.now();
+          const elapsedMs = endTime - startTime;
+          const elapsedUserMs = end.user / 1000;
+          const elapsedSystemMs = end.system / 1000; 
+
+          const cpuPercent =
+            ((elapsedUserMs + elapsedSystemMs) /
+              (elapsedMs * os.cpus().length)) *
+            100;
+          resolve(cpuPercent.toFixed(2)); 
+        }, 100); 
+      });
+    }
+
     try {
-      const options = {
-        month: "long",
-        day: "numeric",
-        year: "numeric",
-        hour: "numeric",
-        minute: "numeric",
-        hour12: true,
-      };
-
-      const response = await axios
-        .get("https://discordstatus.com/api/v2/incidents.json")
-        .catch(function (error) {
-          if (error.response) {
-            console.log(error.response.data);
-            console.log(error.response.status);
-            console.log(error.response.headers);
-          } else if (error.request) {
-            console.log(error.request);
-          } else {
-            console.log("Error", error.message);
-          }
-          console.log(error.config);
-        });
-      const data = response.data;
-
-      function isSameDay(d1, d2) {
-        return (
-          d1.getUTCFullYear() === d2.getUTCFullYear() &&
-          d1.getUTCMonth() === d2.getUTCMonth() &&
-          d1.getUTCDate() === d2.getUTCDate()
-        );
-      }
-
-      const today = new Date();
-      const todaysIncidents = data.incidents.filter((incident) =>
-        isSameDay(new Date(incident.created_at), today)
+      let totalCommits = await getTotalCommits(
+        "Sdriver1",
+        "Pridebot",
+        process.env.githubToken
       );
 
-      let DiscordApiIncident = "No incidents found";
+      let commitTens = totalCommits.toString().slice(-2, -1) || "0";
+      let commitOnes = totalCommits.toString().slice(-1);
 
-      if (todaysIncidents.length) {
-        DiscordApiIncident = todaysIncidents
-          .map((incident) => {
-            const formattedDate = new Date(
-              incident.created_at
-            ).toLocaleDateString("en-US", {
-              month: "long",
-              day: "numeric",
-              year: "numeric",
-            });
-            return `${formattedDate} - [${incident.name}](${data.page.url}/incidents/${incident.id})`;
-          })
-          .join("\n");
-      } else if (data.incidents.length) {
-        const latestIncident = data.incidents[0];
-        const formattedDate = new Date(
-          latestIncident.created_at
-        ).toLocaleDateString("en-US", {
-          month: "long",
-          day: "numeric",
-          year: "numeric",
-        });
-        DiscordApiIncident = `${formattedDate} - [${latestIncident.name}](${data.page.url}/incidents/${latestIncident.id})`;
-      }
-      //-----------------------------------------------------------------------------
-      const repoOwner = "Sdriver1";
-      const repoName = "Pridebot";
-
-      const commitsResponse = await axios.get(
-        `https://api.github.com/repos/${repoOwner}/${repoName}/commits?per_page=100`,
-        {
-          headers: {
-            Authorization: `Bearer ${githubToken}`,
-            Accept: "application/vnd.github.v3+json",
-          },
-        }
-      );
-      const commitsData = commitsResponse.data;
-      const commitCount = commitsData.length;
-      const devcommitCount = commitsData.length - 50;
-
-      let commitTens = "0";
-      let commitOnes = "0";
-
-      let commitsText = `**Commit Count:** ${commitCount}\n`;
-
-      if (commitCount > 0) {
-        const latestCommit = commitsData[0];
-        const latestCommitDate = new Date(
-          latestCommit.commit.author.date
-        ).toLocaleDateString("en-US", {
-          month: "long",
-          day: "numeric",
-          year: "numeric",
-        });
-        const latestCommitLink = latestCommit.html_url;
-        const latestCommitTitle = latestCommit.commit.message;
-        const commitCountStr = devcommitCount.toString();
-        commitTens = commitCountStr.length > 1 ? commitCountStr[0] : "0";
-        commitOnes =
-          commitCountStr.length > 0
-            ? commitCountStr[commitCountStr.length - 1]
-            : "0";
-
-        commitsText += `${latestCommitDate} - [${latestCommitTitle}](${latestCommitLink})`;
-      }
-      //---------------------------------------------------------------------------------------------------
       const currentGuildCount = client.guilds.cache.size;
       let totalUserCount = 0;
       client.guilds.cache.forEach((guild) => {
         totalUserCount += guild.memberCount;
       });
 
+      const cpuPercent = await getCpuUsage();
+      const memoryUsage = `${(
+        process.memoryUsage().heapUsed /
+        1024 /
+        1024
+      ).toFixed(2)} MB`;
+
       const formattedTotalUserCount = totalUserCount.toLocaleString();
-      //--------------------------------------------------------------------------------
-
-      async function getRegisteredCommandsCount(client) {
-        if (!client.application) {
-          console.error("Client application is not ready.");
-          return 0;
-        }
-        const commands = await client.application.commands.fetch();
-        return commands.size;
-      }
-
       const CommandsCount = (await getRegisteredCommandsCount(client)) + 2;
-
-      //--------------------------------------------------------------------------------
-
-      const Client = new Nodeactyl.NodeactylClient(
-        "https://panel.sneakyhost.xyz/",
-        apikey
-      );
-
-      let serviceMem = "Unavailable";
-      let serviceCPU = "Unavailable";
-      let serviceDk = "Unavailable";
-
-      try {
-        const serverStats = await Client.getServerUsages("2e12ab3b");
-
-        const memoryMB = serverStats.resources.memory_bytes / 1024 / 1024;
-        const cpuPercentage = serverStats.resources.cpu_absolute;
-        const diskMB = serverStats.resources.disk_bytes / 1024 / 1024;
-
-        serviceMem = `**RAM:** \`${memoryMB.toFixed(2)} MiB / 700 MiB\``;
-        serviceCPU = `**CPU:** \`${cpuPercentage.toFixed(2)}%\``;
-        serviceDk = `**Disk:** \`${diskMB.toFixed(2)} MiB / 1.22 GiB\``;
-      } catch (error) {
-        console.error("Error fetching server stats:", error);
-      }
-
-      //--------------------------------------------------------------------------------
-
+      const profileAmount = await Profile.countDocuments();
       const usages = await CommandUsage.find({}).sort({ count: -1 });
       const totalUsage = usages.reduce((acc, cmd) => acc + cmd.count, 0);
-
-      //--------------------------------------------------------------------------------
-
       const startTimeTimestamp = `<t:${client.botStartTime}:f>`;
 
       const ping = `**Ping**: \`${botping}ms\` \n**API Latency**: \`${client.ws.ping}\``;
-
       const up = `\n**Uptime:** \`${formatUptime(
         process.uptime()
-      )} \` \n**Start Time:** ${startTimeTimestamp}`;
+      )}\` \n**Start Time:** ${startTimeTimestamp}`;
+      const botstats = `**Servers:** \`${currentGuildCount}\` \n**Users:** \`${formattedTotalUserCount}\``;
+      const commandstats = `**Commands:** \`${CommandsCount}\` \n**Total Usage:** \`${totalUsage}\` \n**Profiles:** \`${profileAmount}\``;
+      const botversion = `**Dev:** \`2.${commitTens}.${commitOnes}\` \n **Node.js:** \`${process.version}\` \n **Discord.js:** \`v14.14.1\``;
+      const clientstats = `**CPU:** ${cpuPercent}% / 100 \n**Memory:** ${memoryUsage} / 6 GB`;
 
-      const botstats = `**Version:** \`2.${commitTens}.${commitOnes}\` \n**Guilds:** \`${currentGuildCount}\` \n**Users:** \`${formattedTotalUserCount}\` \n**Commands:** \`${CommandsCount}\` \n**Commands Used:** \`${totalUsage}\``;
-
-      const servicestats = `${serviceMem} \n${serviceCPU} \n${serviceDk}`;
-
-      try {
-        const embed = new EmbedBuilder().setColor(0xff00ae).addFields(
+      const embed = new EmbedBuilder()
+        .setDescription(
+          "# <:_:1108228682184654908> Pridebot Stats \n Here are some stats about Pridebot!"
+        )
+        .setColor(0xff00ae)
+        .addFields(
           {
-            name: "<:_:1108228682184654908> __Bot Stats__",
+            name: "<:_:1195874659338555462> __Servers/Users__",
             value: botstats,
             inline: true,
           },
           {
-            name: "<:_:1108417509624926228> __Bot Uptime__",
-            value: up,
-            inline: true,
-          },
-          {
-            name: "<:_:1191202343505645690> __Bot Ping__",
+            name: "<:_:1191202343505645690> __Ping/Latency__",
             value: ping,
             inline: true,
           },
           {
-            name: "<:_:1193823319246524486> __Bot Usage__",
-            value: servicestats,
+            name: "<:_:1115832874143322122> __Usage__",
+            value: clientstats,
             inline: true,
           },
           {
-            name: "<:_:1108421476148859010> __Latest Discord API Incident__",
-            value: DiscordApiIncident,
+            name: "<:_:1115831076993110067> __Command/Profile__",
+            value: commandstats,
+            inline: true,
+          },
+          {
+            name: "<:_:1112602480128299079> __Versions__",
+            value: botversion,
+            inline: true,
+          },
+          {
+            name: "<:_:1108417509624926228> __Uptime__",
+            value: up,
             inline: true,
           }
-        );
+        )
+        .setTimestamp();
 
-        await interaction.editReply({ embeds: [embed] });
-      } catch (error) {
-        console.error("Error sending edit reply:", error);
-        if (error.code !== "InteractionAlreadyReplied") {
-          await interaction.followUp({
-            content: "There was an error processing your request.",
-            ephemeral: true,
-          });
-        }
-      }
+      await interaction.editReply({ embeds: [embed] });
     } catch (error) {
       console.error("Error deferring reply:", error);
     }
   },
 };
 
-function formatUptime(time) {
+async function getTotalCommits(repoOwner, repoName, githubToken) {
+  let page = 1;
+  let totalCommits = 0;
+
+  while (true) {
+    const url = `https://api.github.com/repos/${repoOwner}/${repoName}/commits?per_page=100&page=${page}`;
+    const commitsResponse = await axios.get(url, {
+      headers: {
+        Authorization: `Bearer ${githubToken}`,
+        Accept: "application/vnd.github.v3+json",
+      },
+    });
+    const commits = commitsResponse.data;
+    if (commits.length === 0) {
+      break;
+    }
+    totalCommits += commits.length;
+    page++;
+  }
+
+  return totalCommits;
+}
+
+async function getRegisteredCommandsCount(client) {
+  if (!client.application?.id) {
+    console.error("Client application is not ready.");
+    return 0;
+  }
+  const commands = await client.application.commands.fetch();
+  return commands.size;
+}
+
+function formatUptime(seconds) {
   const timeUnits = {
     day: 3600 * 24,
     hour: 3600,
     minute: 60,
     second: 1,
   };
-  return Object.entries(timeUnits)
-    .reduce((acc, [unit, unitSeconds]) => {
-      const amount = Math.floor(time / unitSeconds);
-      time -= amount * unitSeconds;
-      return amount ? [...acc, `${amount} ${unit}(s)`] : acc;
-    }, [])
-    .join(" ");
-}
-
-function formatTimestamp(timestamp) {
-  const dateObj = new Date(timestamp);
-  if (isNaN(dateObj)) {
-    const dateMatch = /(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2})/.exec(timestamp);
-    if (dateMatch) {
-      const [_, year, month, day, hour, minute] = dateMatch;
-      return `${year}-${padZero(month)}-${padZero(day)} ${padZero(
-        hour
-      )}:${padZero(minute)}`;
+  let result = [];
+  for (const [unit, amountInSeconds] of Object.entries(timeUnits)) {
+    const quantity = Math.floor(seconds / amountInSeconds);
+    seconds %= amountInSeconds;
+    if (quantity > 0) {
+      result.push(`${quantity} ${unit}${quantity > 1 ? "s" : ""}`);
     }
-    return "Invalid Date";
   }
-  return dateObj.toLocaleString();
-}
-
-function padZero(value) {
-  return value.toString().padStart(2, "0");
+  return result.join(", ");
 }
